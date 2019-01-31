47c3210fa6209be6e358a7bc4ddbf3a203bd2b2f
Joanne U <joanne.u@gmx.ch>
2019-01-31 12:54:48 +0100

    Create RepeatingBG.cs

---
+++ RepeatingBG.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBG : MonoBehaviour
+{
+
+    public float speed;
+    public float endX;
+    public float startX;
+    public float maxSpeed;
+
+    private void Update()
+    {
+        //Background nach links
+        if (speed < maxSpeed)
+        {
+            speed = Mathf.Min(speed + .0015f);
+        }
+
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        //zurück zum Start
+        if (transform.position.x <= endX)
+        {
+            Vector2 pos = new Vector2(startX, transform.position.y);
+            transform.position = pos;
+        }
+    }
+}
