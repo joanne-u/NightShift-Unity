244c7a35611d3ecadc4ca3265c6f2655c13076b7
Joanne U <joanne.u@gmx.ch>
2019-01-31 12:54:52 +0100

    Create ScoreManager.cs

---
+++ ScoreManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public Text scoreText;
+    public Text hiScoreText;
+
+    //public float scoreCount;
+    public static int scoreCount = 0;
+    public static int hiScoreCount = 0;
+
+    //löschen
+    //public float pointsPerSecond;
+
+    public bool scoreIncreasing;
+
+
+    //// Use this for initialization
+    void Start () {
+
+    }
+
+    //// Update is called once per frame
+    void Update () {
+
+        if (scoreCount > hiScoreCount)
+        {
+            hiScoreCount = scoreCount;
+        }
+
+        scoreText.text = "Score: " + scoreCount;
+        hiScoreText.text = "High Score: " + hiScoreCount;
+
+
+    }
+}
