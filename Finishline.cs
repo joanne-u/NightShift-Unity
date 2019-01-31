e83776593dc8c0cf9c60efd4012c977778528d1f
Joanne U <joanne.u@gmx.ch>
2019-01-31 12:54:40 +0100

    Create Finishline.cs

---
+++ Finishline.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finishline : MonoBehaviour
+{
+    public float speed;
+    float time = 60f;
+
+    void Update()
+    {
+        if (time >= 0)
+        {
+            time -= Time.deltaTime;
+            return;
+        }
+        else
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
+
+
+
+
