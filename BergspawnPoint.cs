fb449bc2bd2212c5c58da312b9346f4e880e9ebe
Joanne U <joanne.u@gmx.ch>
2019-01-31 12:54:23 +0100

    Create BergspawnPoint.cs

---
+++ BergspawnPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BergspawnPoint : MonoBehaviour
+{
+
+    public GameObject Bergobstacle;
+
+    void Start()
+    {
+        Instantiate(Bergobstacle, transform.position, Quaternion.identity);
+    }
+}
