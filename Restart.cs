3f56fa82b9d9c1adedfa8d7e40e287a6897dc510
Joanne U <joanne.u@gmx.ch>
2019-01-31 12:54:50 +0100

    Create Restart.cs

---
+++ Restart.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+
+    public GameObject MainMenu;
+
+    public void Update()
+    {
+        //Restart
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //Score resetten auf mainmenu value
+            ScoreManager.scoreCount = PlayerPrefs.GetInt("scoreNew");
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        }
+        //Menü
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            ScoreManager.scoreCount = PlayerPrefs.GetInt("scoreNew");
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+        //Quit
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
