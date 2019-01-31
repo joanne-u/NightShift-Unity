edb873f376673a5ddb2a95fa2c6cffa384ebc086
Joanne U <joanne.u@gmx.ch>
2019-01-31 12:54:42 +0100

    Create MainMenu.cs

---
+++ MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    bool isMute;
+
+    public void PlayGame()
+    {
+        PlayerPrefs.SetInt("scoreNew", ScoreManager.scoreCount);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void MuteGame()
+    {
+        //Muten
+        isMute = !isMute;
+        AudioListener.volume = isMute ? 0 : 1;
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void PlayGameStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        ScoreManager.scoreCount = 0;
+        PlayerPrefs.SetInt("scoreNew", 0);
+    }
+}
