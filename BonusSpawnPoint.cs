90f11dd9c561f2dc234a5c195f982915569b0df2
Joanne U <joanne.u@gmx.ch>
2019-01-31 12:54:32 +0100

    Create BonusSpawnPoint.cs

---
+++ BonusSpawnPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusSpawnPoint : MonoBehaviour
+{
+
+    public GameObject BonusObstacle;
+
+    void Start()
+    {
+        Instantiate(BonusObstacle, transform.position, Quaternion.identity);
+    }
+}
