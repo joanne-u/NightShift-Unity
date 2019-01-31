bb3be779f671c2c0360c641e10fff3916c058451
Joanne U <joanne.u@gmx.ch>
2019-01-31 12:54:36 +0100

    Create disable.cs

---
+++ disable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+
+public class disable : MonoBehaviour
+{
+
+    public Button Mute;
+    public Sprite muteA;
+    public Sprite muteA_disable;
+    private int counter = 0;
+
+    void Start()
+    {
+        Mute = GetComponent<Button>();
+    }
+
+    public void changeButton()
+    {
+        counter++;
+        if (counter % 2 == 0)
+        {
+            Mute.image.overrideSprite = muteA;
+        }
+        else
+        {
+            Mute.image.overrideSprite = muteA_disable;
+
+        }
+    }
+}
