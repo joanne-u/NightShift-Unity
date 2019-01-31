04774e703c32f8a6bdefd81b1cea19cb9404be06
Joanne U <joanne.u@gmx.ch>
2019-01-31 12:55:00 +0100

    Create Spawner.cs

---
+++ Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] obstaclePatterns;
+
+    private float timeBtwSpawn;
+    public float startTimeBtwSpawn;
+    public float decreaseTime;
+    public float minTime = 0.6f;
+
+    public float cloudSpeed = 5.0f;
+
+    private void Update()
+    {
+        if (timeBtwSpawn <= 0)
+        {
+            cloudSpeed = Mathf.Min(cloudSpeed + .1f, 8.5f);
+
+            //randomisieren
+            int rand = Random.Range(0, obstaclePatterns.Length);
+            Instantiate<GameObject>(obstaclePatterns[rand], transform.position, Quaternion.identity);
+            timeBtwSpawn = startTimeBtwSpawn;
+
+            //Spawn-Zeit mit zunehmender Spielzeit abnehmen
+            if (startTimeBtwSpawn > minTime)
+            {
+                startTimeBtwSpawn -= decreaseTime;
+            }
+        }
+        else
+        {
+            timeBtwSpawn -= Time.deltaTime;
+
+        }
+    }
+}
