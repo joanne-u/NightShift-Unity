682f791e11542268715b361390e52b157c04f454
Joanne U <joanne.u@gmx.ch>
2019-01-31 12:54:29 +0100

    Create BonusObstacleLight.cs

---
+++ BonusObstacleLight.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusObstacleLight : MonoBehaviour
+{
+    public float currentBonusSpeed;
+    public float accelerationBonusTime = 30;
+    private float minBonusSpeed;
+
+    private bool hasCollided = false;
+
+    public float speed;
+    public float fallSpeed = 80.0f;
+
+    //Bonus Sound
+    public GameObject bonusCollision;
+
+    private void Start()
+    {
+        currentBonusSpeed = GameObject.Find("/BonusSpawner").GetComponent<BonusSpawner>().bonusSpeed;
+    }
+
+    private void Update()
+    {
+        //Obstacle auf Player zubewegen
+        transform.Translate(Vector2.left * currentBonusSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !hasCollided)
+        {
+            //Sound
+            Instantiate(bonusCollision, transform.position, Quaternion.identity);
+
+            Destroy(gameObject);
+
+            ScoreManager.scoreCount += 1;
+            hasCollided = true;
+        }
+    }
+}
